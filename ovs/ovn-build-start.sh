0bc37cda0a63bc1bcf1be871426d388dfec0c231
Pengfei Ni <feiskyer@users.noreply.github.com>
2017-04-28 13:33:39 +0800

    Update and rename ovs-build.sh to ovn-build-start.sh

--- ovs/ovn-build-start.sh
+++ ovs/ovn-build-start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# build, install and start ovn
 
 download-ovs() {
   curl -o openvswitch-2.7.0.tar.gz http://openvswitch.org/releases/openvswitch-2.7.0.tar.gz
@@ -54,6 +55,8 @@ start-ovn() {
   export LOCAL_IP=${CENTRAL_IP}
   export ENCAP_TYPE=vxlan
   ovs-vsctl set Open_vSwitch . external_ids:ovn-remote="tcp:$CENTRAL_IP:6642" external_ids:ovn-nb="tcp:$CENTRAL_IP:6641" external_ids:ovn-encap-ip=$LOCAL_IP external_ids:ovn-encap-type="$ENCAP_TYPE"
+  ovn-nbctl set-connection ptcp:6641
+  ovn-sbctl set-connection ptcp:6642
 }
 
 
