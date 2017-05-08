6ee38d2e3d89fef1c1871d993ac38b28a9983335
Pengfei Ni <feiskyer@users.noreply.github.com>
2017-05-08 12:02:07 +0800

    Update setup-dockershim.sh

--- kubernetes/CRI/setup-dockershim.sh
+++ kubernetes/CRI/setup-dockershim.sh
@@ -3,5 +3,4 @@ git clone https://github.com/kubernetes/kubernetes $GOPATH/src/k8s.io/kubernetes
 cd $GOPATH/src/k8s.io/kubernetes
 make WHAT='cmd/kubelet'
 
-screen -dmS dockershim _outpub/bin/kubelet --v=3 --logtostderr --experimental-dockershim 
-
+screen -dmS dockershim _output/bin/kubelet --v=3 --logtostderr --experimental-dockershim
