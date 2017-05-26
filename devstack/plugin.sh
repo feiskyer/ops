71b5deffa9699b5addf34ea944b2cad91a863620
Pengfei Ni <feiskyer@gmail.com>
2017-05-26 18:37:31 +0800

    update

--- devstack/plugin.sh
+++ devstack/plugin.sh
@@ -2,8 +2,8 @@
 STACKUBE_ROOT=$(dirname "${BASH_SOURCE}")
 
 function configure_cni {
-    mkdir -p /etc/cni/net.d
-    cat >/etc/cni/net.d/10-mynet.conf <<-EOF
+    sudo mkdir -p /etc/cni/net.d
+    sudo sh -c 'cat >/etc/cni/net.d/10-mynet.conf <<EOF
 {
     "cniVersion": "0.3.0",
     "name": "mynet",
@@ -19,25 +19,21 @@ function configure_cni {
         ]
     }
 }
-EOF
-    cat >/etc/cni/net.d/99-loopback.conf <<-EOF
+EOF'
+    sudo sh -c 'cat >/etc/cni/net.d/99-loopback.conf <<EOF
 {
     "cniVersion": "0.3.0",
     "type": "loopback"
 }
-EOF
+EOF'
 }
 
 function install_docker {
     if is_ubuntu; then
-        apt-get update
-        apt-get install -y docker.io
-        systemctl enable docker
-        systemctl start docker
+        sudo apt-get update
+        sudo apt-get install -y docker.io
     elif is_fedora; then
-        yum install -y docker
-        systemctl enable docker
-        systemctl start docker
+        sudo yum install -y docker
     else
         exit_distro_not_supported
     fi
@@ -45,29 +41,29 @@ function install_docker {
 
 function install_hyper {
     if is_ubuntu; then
-        apt-get update && apt-get install -y qemu libvirt-bin
+        sudo apt-get update && apt-get install -y qemu libvirt-bin
     elif is_fedora; then
-        yum install -y libvirt
+        sudo yum install -y libvirt
     fi
 
-    curl -sSL https://hypercontainer.io/install | bash
-    echo -e "Kernel=/var/lib/hyper/kernel\n\
-Initrd=/var/lib/hyper/hyper-initrd.img\n\
-Hypervisor=qemu\n\
-StorageDriver=overlay\n\
-gRPCHost=127.0.0.1:22318" > /etc/hyper/config
-    systemctl enable hyperd
-    systemctl restart hyperd
+    curl -sSL https://hypercontainer.io/install | sudo bash
+    sudo sh -c 'cat>/etc/hyper/config <<EOF
+Kernel=/var/lib/hyper/kernel
+Initrd=/var/lib/hyper/hyper-initrd.img
+Hypervisor=qemu
+StorageDriver=overlay
+gRPCHost=127.0.0.1:22318"
+EOF'
 }
 
 function install_frakti {
     if command -v /usr/bin/frakti > /dev/null 2>&1; then
-        rm -f /usr/bin/frakti
+        sudo rm -f /usr/bin/frakti
     fi
-    curl -sSL https://github.com/kubernetes/frakti/releases/download/${FRAKTI_VERSION}/frakti -o /usr/bin/frakti
-    chmod +x /usr/bin/frakti
+    sudo curl -sSL https://github.com/kubernetes/frakti/releases/download/${FRAKTI_VERSION}/frakti -o /usr/bin/frakti
+    sudo chmod +x /usr/bin/frakti
     cgroup_driver=$(docker info | awk '/Cgroup Driver/{print $3}')
-    cat <<EOF > /lib/systemd/system/frakti.service
+    sudo sh -c 'cat > /lib/systemd/system/frakti.service <<EOF
 [Unit]
 Description=Hypervisor-based container runtime for Kubernetes
 Documentation=https://github.com/kubernetes/frakti
@@ -89,14 +85,12 @@ TimeoutStartSec=0
 Restart=on-abnormal
 [Install]
 WantedBy=multi-user.target
-EOF
-    systemctl enable frakti
-    systemctl start frakti
+EOF'
 }
 
 function install_kubelet {
     if is_fedora; then
-        cat <<EOF > /etc/yum.repos.d/kubernetes.repo
+        sudo sh -c 'cat > /etc/yum.repos.d/kubernetes.repo <<EOF
 [kubernetes]
 name=Kubernetes
 baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
@@ -105,25 +99,25 @@ gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOF
-        setenforce 0
-        sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
-        yum install -y kubernetes-cni kubelet kubeadm kubectl
+EOF'
+        sudo setenforce 0
+        sudo sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
+        sudo yum install -y kubernetes-cni kubelet kubeadm kubectl
     elif is_ubuntu; then
-        apt-get update && apt-get install -y apt-transport-https
-        curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-        cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
+        sudo apt-get update && apt-get install -y apt-transport-https
+        sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+        sudo sh -c 'cat > /etc/apt/sources.list.d/kubernetes.list <<EOF 
 deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-        apt-get update
-        apt-get install -y kubernetes-cni kubelet kubeadm kubectl
+EOF'
+        sudo apt-get update
+        sudo apt-get install -y kubernetes-cni kubelet kubeadm kubectl
     else
         exit_distro_not_supported
     fi
 }
 
 function install_master {
-    kubeadm init kubeadm init --pod-network-cidr ${CLUSTER_CIDR} --config ${STACKUBE_ROOT}/kubeadm.yaml
+    sudo kubeadm init kubeadm init --pod-network-cidr ${CLUSTER_CIDR} --config ${STACKUBE_ROOT}/kubeadm.yaml
     # Enable schedule pods on the master for testing.
     export KUBECONFIG=/etc/kubernetes/admin.conf
     kubectl taint nodes --all node-role.kubernetes.io/master-
@@ -134,27 +128,27 @@ function install_node {
         echo "KUBEADM_TOKEN must be set for node"
         exit 1
     fi
-    kubeadm join --token "${KUBEADM_TOKEN}" ${KUBERNETES_MASTER_IP}:${KUBERNETES_MASTER_PORT}
+    sudo kubeadm join --token "${KUBEADM_TOKEN}" ${KUBERNETES_MASTER_IP}:${KUBERNETES_MASTER_PORT}
 }
 
 function configure_kubelet {
-    sed -i '2 i\Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --container-runtime-endpoint=/var/run/frakti.sock --feature-gates=AllAlpha=true"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
-    systemctl daemon-reload
+    sudo sed -i '2 i\Environment="KUBELET_EXTRA_ARGS=--container-runtime=remote --container-runtime-endpoint=/var/run/frakti.sock --feature-gates=AllAlpha=true"' /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+    sudo systemctl daemon-reload
 }
 
 function remove_kubernetes {
-    kubeadm reset
-    systemctl stop kubelet
+    sudo kubeadm reset
+    sudo systemctl stop kubelet
 
     if is_fedora; then
-        yum remove -y qemu-hyper hyperstart hyper-container libvirt
-        yum remove -y kubernetes-cni kubelet kubeadm kubectl docker
+        sudo yum remove -y qemu-hyper hyperstart hyper-container libvirt
+        sudo yum remove -y kubernetes-cni kubelet kubeadm kubectl docker
     elif is_ubuntu; then
-        apt-get remove -y hyperstart hyper-container qemu libvirt-bin
-        apt-get remove -y kubernetes-cni kubelet kubeadm kubectl docker
+        sudo apt-get remove -y hyperstart hyper-container qemu libvirt-bin
+        sudo apt-get remove -y kubernetes-cni kubelet kubeadm kubectl docker
     fi
 
-    rm -rf /usr/bin/frakti /etc/cni/net.d /lib/systemd/system/frakti.service
+    sudo rm -rf /usr/bin/frakti /etc/cni/net.d /lib/systemd/system/frakti.service
 }
 
 function install_stackube {
@@ -164,6 +158,10 @@ function install_stackube {
 }
 
 function init_stackube {
+    sudo systemctl start docker
+    sudo systemctl restart hyperd
+    sudo systemctl start frakti
+
     if is_service_enabled kubernetes_master; then
         install_master
     elif is_service_enabled kubernetes_node; then
