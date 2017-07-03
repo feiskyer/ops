e7fff938009b24f5ff6a8ae31b12aca23f31e313
Pengfei Ni <feiskyer@users.noreply.github.com>
2017-07-03 10:26:42 +0800

    Update kubernetes.sh

--- kubernetes/lib/kubernetes.sh
+++ kubernetes/lib/kubernetes.sh
@@ -35,7 +35,7 @@ EOF
 }
 
 setup-master() {
-    kubeadm init kubeadm init --pod-network-cidr ${CLUSTER_CIDR} --config ${KUBERNTES_LIB_ROOT}/kubeadm.yaml
+    kubeadm init --pod-network-cidr ${CLUSTER_CIDR} --config ${KUBERNTES_LIB_ROOT}/kubeadm.yaml
     # create default host-path storage class
     # kubectl create -f ${KUBERNTES_LIB_ROOT}/storage-class.yaml
     # Also enable schedule pods on the master for allinone.
