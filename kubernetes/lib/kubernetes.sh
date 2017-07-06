4ea00ec613575138b8fd0f92ca28eebbd6e8d922
Pengfei Ni <feiskyer@users.noreply.github.com>
2017-07-06 15:32:02 +0800

    Update kubernetes.sh

--- kubernetes/lib/kubernetes.sh
+++ kubernetes/lib/kubernetes.sh
@@ -11,12 +11,12 @@ install-kubelet-centos() {
     cat <<EOF > /etc/yum.repos.d/kubernetes.repo
 [kubernetes]
 name=Kubernetes
-baseurl=http://yum.kubernetes.io/repos/kubernetes-el7-x86_64
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-x86_64
 enabled=1
 gpgcheck=1
 repo_gpgcheck=1
 gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+        https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOF
     setenforce 0
     sed -i 's/SELINUX=enforcing/SELINUX=disabled/g' /etc/selinux/config
