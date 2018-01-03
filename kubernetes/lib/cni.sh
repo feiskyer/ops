1ab4c6c850bdf30d72e7a3b493be83dfa1202ec3
Pengfei Ni <feiskyer@gmail.com>
2018-01-03 14:04:57 +0800

    update calico

--- kubernetes/lib/cni.sh
+++ kubernetes/lib/cni.sh
@@ -12,7 +12,9 @@ install-flannel() {
 }
 
 install-calico() {
-    curl -O -L https://docs.projectcalico.org/v2.4/getting-started/kubernetes/installation/hosted/kubeadm/1.6/calico.yaml
+    kubectl apply -f https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/rbac-kdd.yaml
+    curl -O -L https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubernetes-datastore/calico-networking/1.7/calico.yaml
+    # curl -O -L https://docs.projectcalico.org/v3.0/getting-started/kubernetes/installation/hosted/kubeadm/1.7/calico.yaml
     sed -i -e 's/192\.168/10.244/' calico.yaml
     kubectl apply -f calico.yaml
 }
