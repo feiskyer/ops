51aa8864f9b60af57a423e974475bcab6d5409e2
Pengfei Ni <peni@microsoft.com>
2018-01-08 15:49:46 +0800

    Add --ignore-preflight-errors

--- kubernetes/lib/kubernetes.sh
+++ kubernetes/lib/kubernetes.sh
@@ -35,11 +35,16 @@ EOF
     apt-get install -y kubernetes-cni kubelet kubeadm kubectl
 }
 
+config-kubelet-containerd() {
+	echo -e '[Service]\nEnvironment="KUBELET_EXTRA_ARGS=--container-runtime=remote --runtime-request-timeout=15m --container-runtime-endpoint=/var/run/cri-containerd.sock"\n' | sudo tee /etc/systemd/system/kubelet.service.d/0-cri-containerd.conf
+	sudo systemctl daemon-reload
+}
+
 setup-master() {
     # Sometime /var/lib/kubelet is not empty after kubelet installation.
     rm -rf /var/lib/kubelet
     # Setup master
-    kubeadm init --config ${KUBERNTES_LIB_ROOT}/kubeadm.yaml
+    kubeadm init --config ${KUBERNTES_LIB_ROOT}/kubeadm.yaml --ignore-preflight-errors all
     # create default host-path storage class
     # kubectl create -f ${KUBERNTES_LIB_ROOT}/storage-class.yaml
     # Also enable schedule pods on the master for allinone.
