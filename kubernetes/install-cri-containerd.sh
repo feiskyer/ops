3f8ac29dc7b56e4e760325ee4208f0ef49562040
Pengfei Ni <peni@microsoft.com>
2018-01-08 15:50:40 +0800

    Update cri containerd installation

--- kubernetes/install-cri-containerd.sh
+++ kubernetes/install-cri-containerd.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 # Install cri-containerd.
+VERSION="1.0.0-beta.0"
+sudo apt-get update && sudo apt-get install libseccomp2 -y
+wget https://storage.googleapis.com/cri-containerd-release/cri-containerd-${VERSION}.linux-amd64.tar.gz
 
-git clone https://github.com/kubernetes-incubator/cri-containerd $GOPATH/src/github.com/kubernetes-incubator/cri-containerd
-cd $GOPATH/src/github.com/kubernetes-incubator/cri-containerd
-hack/install-deps.sh 
-make 
-sudo make install
-
-sudo cp contrib/systemd-units/* /lib/systemd/system/
-sudo systemctl daemon-reload
-
+sudo tar -C / -xzf cri-containerd-${VERSION}.linux-amd64.tar.gz
+sudo mkdir -p /opt/cni/bin/
+sudo mkdir -p /etc/cni/net.d
 sudo systemctl start containerd
 sudo systemctl start cri-containerd
-sudo systemctl enable containerd
-sudo systemctl enable cri-containerd
 
