232ded647e32768691c936f752e737f6bff4c119
Pengfei Ni <feiskyer@users.noreply.github.com>
2018-04-23 10:19:56 +0800

    upgrade golang version

--- misc/golang-install.sh
+++ misc/golang-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GOVERSION=1.9.2
+GOVERSION=1.10.1
 curl -sL https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz | tar -C /usr/local -zxf -
 
 echo 'export GOPATH=/go' >> ~/.bashrc
