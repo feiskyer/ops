7fcb11379000cce687ec6f803e0a40ae66ccae76
Pengfei Ni <peni@microsoft.com>
2019-01-07 14:35:40 +0800

    update go version to 1.11.4

--- misc/golang-install.sh
+++ misc/golang-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-GOVERSION=1.11.2
+GOVERSION=1.11.4
 curl -sL https://storage.googleapis.com/golang/go$GOVERSION.linux-amd64.tar.gz | sudo tar -C /usr/local -zxf -
 
 echo 'export GOPATH=/go' >> ~/.bashrc
