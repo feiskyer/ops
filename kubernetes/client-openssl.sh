1f3f95af30fa33862342bc56ad3fc12fcbdb3335
Pengfei Ni <feiskyer@users.noreply.github.com>
2019-12-29 18:54:16 +0800

    Update client-openssl.sh

--- kubernetes/client-openssl.sh
+++ kubernetes/client-openssl.sh
@@ -1,23 +1,17 @@
 #!/bin/bash
 # setup kubectl via openssl
-#
-SERVER_URL=${SERVER_URL:-"https://127.0.0.1:6443"}
-NAMESPACE=${NAMESPACE:-"demo"}
-USER=${USER:-"demo"}
-GROUP=${GROUP:-"demo"}
+
+NAMESPACE=${NAMESPACE:-"default"}
+USER_NAME=${USER_NAME:-"user1"}
+GROUP_NAME=${GROUP_NAME:-"group1"}
+SERVER_URL=$(kubectl cluster-info | awk '/Kubernetes master/{print $NF}' | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g')
 
 # create client key and cert 
-openssl genrsa -out $USER.key 2048
-openssl req -new -key $USER.key -out $USER.csr -subj "/CN=$USER/O=$GROUP"
+openssl genrsa -out $USER_NAME.key 2048
+openssl req -new -key $USER_NAME.key -out $USER_NAME.csr -subj "/CN=$USER_NAME/O=$GROUP_NAME"
 
 # Sign the client certificates
-# 
-# * Sign method I: using existing cluster certs *
-# openssl x509 -req -in $USER.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out $USER.crt -days 365
-# openssl x509 -in $USER.crt -clrtrust -out $USER.crt
-#
-# * Sign method II: using CertificateSigningRequest *
-CERTIFICATE_NAME=$USER.$NAMESPACE
+CERTIFICATE_NAME=$USER_NAME-$NAMESPACE
 cat <<EOF | kubectl create -f -
 apiVersion: certificates.k8s.io/v1beta1
 kind: CertificateSigningRequest
@@ -26,23 +20,22 @@ metadata:
 spec:
   groups:
   - system:authenticated
-  request: $(cat $USER.csr | base64 | tr -d '\n')
+  request: $(cat $USER_NAME.csr | base64 | tr -d '\n')
   usages:
   - digital signature
   - key encipherment
-  - server auth
+  - client auth
 EOF
 kubectl certificate approve $CERTIFICATE_NAME
-kubectl get csr $CERTIFICATE_NAME -o jsonpath='{.status.certificate}'  | base64 -D > $USER.crt
+kubectl get csr $CERTIFICATE_NAME -o jsonpath='{.status.certificate}'  | base64 --decode > $USER_NAME.crt
 
-
-# setup role
+# setup RBAC Roles
 cat <<EOF | kubectl create -f -
 kind: Role
-apiVersion: rbac.authorization.k8s.io/v1alpha1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   namespace: $NAMESPACE
-  name: $USER-role
+  name: $USER_NAME-role
 rules:
   - apiGroups:
     - ''
@@ -56,10 +49,10 @@ rules:
 EOF
 
 # bind role to the user
-kubectl create rolebinding $USER-rolebinding --user=$USER --namespace=$NAMESPACE --role=$USER-role
+kubectl create rolebinding $USER_NAME-rolebinding --user=$USER_NAME --namespace=$NAMESPACE --role=$USER_NAME-role
 
 # setup kubectl
-kubectl config set-cluster k8s --server="$SERVER_URL" --insecure-skip-tls-verify
-kubectl config set-credentials $USER --client-certificate=$USER.crt --client-key=$USER.key
-kubectl config set-context k8s --cluster=k8s --user=$USER --namespace=$NAMESPACE
-kubectl config use-context k8s
+kubectl config set-cluster $USER_NAME --server="$SERVER_URL" --insecure-skip-tls-verify
+kubectl config set-credentials $USER_NAME --client-certificate=$USER_NAME.crt --client-key=$USER_NAME.key
+kubectl config set-context $USER_NAME --cluster=$USER_NAME --user=$USER_NAME --namespace=$NAMESPACE
+kubectl config use-context $USER_NAME
