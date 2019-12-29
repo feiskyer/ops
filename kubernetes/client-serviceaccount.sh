f65c4d477987af30ebee4b5015d23c25598dcc0d
Pengfei Ni <feiskyer@users.noreply.github.com>
2019-12-29 18:54:51 +0800

    Update client-serviceaccount.sh

--- kubernetes/client-serviceaccount.sh
+++ kubernetes/client-serviceaccount.sh
@@ -1,32 +1,24 @@
 #!/bin/bash
 # setup kubectl via default service account
-#
-SERVER_URL=${SERVER_URL:-"https://127.0.0.1:6443"}
-NAMESPACE=${NAMESPACE:-"demo"}
+NAMESPACE=${NAMESPACE:-"default"}
+SERVICE_ACCOUNT_NAME=${SERVICE_ACCOUNT_NAME:-"demo"}
+SERVER_URL=$(kubectl cluster-info | awk '/Kubernetes master/{print $NF}' | sed $'s,\x1b\\[[0-9;]*[a-zA-Z],,g')
 
-kubectl create namespace $NAMESPACE
-
-# verify default sa has been created.
-kubectl --namespace=$NAMESPACE get sa
+# create sa
+kubectl -n $NAMESPACE create sa $SERVICE_ACCOUNT_NAME
 
 # get secret and token
-secret=$(kubectl -n $NAMESPACE get sa default -o jsonpath='{.secrets[0].name}')
+secret=$(kubectl -n $NAMESPACE get sa $SERVICE_ACCOUNT_NAME -o jsonpath='{.secrets[0].name}')
 token=$(kubectl -n $NAMESPACE get secret $secret -o jsonpath='{.data.token}' | base64 -d)
-kubectl -n $NAMESPACE get secret $secret -o jsonpath='{.data.ca\.crt}' | base64 -d > ca.crt
-
-# setup kubectl
-kubectl config set-cluster kubernetes --embed-certs=true --server=${SERVER_URL} --certificate-authority=./ca.crt
-kubectl config set-credentials $NAMESPACE --token=$token
-kubectl config set-context kubernetes --cluster=kubernetes --user=$NAMESPACE --namespace=$NAMESPACE
-kubectl config use-context kubernetes
+kubectl -n $NAMESPACE get secret $secret -o jsonpath='{.data.ca\.crt}' | base64 --decode > ca.crt
 
 # setup RBAC Roles
 cat <<EOF | kubectl create -f -
 kind: Role
-apiVersion: rbac.authorization.k8s.io/v1alpha1
+apiVersion: rbac.authorization.k8s.io/v1
 metadata:
   namespace: $NAMESPACE
-  name: $NAMESPACE-role
+  name: $SERVICE_ACCOUNT_NAME-role
 rules:
   - apiGroups:
     - ''
@@ -40,30 +32,10 @@ rules:
 EOF
 
 # bind sa to the role
-kubectl create rolebinding $NAMESPACE-rolebinding --serviceaccount=$NAMESPACE:default --namespace=$NAMESPACE --role=$NAMESPACE-role
+kubectl create rolebinding $SERVICE_ACCOUNT_NAME-rolebinding --serviceaccount=$NAMESPACE:$SERVICE_ACCOUNT_NAME --namespace=$NAMESPACE --role=$SERVICE_ACCOUNT_NAME-role
 
-# setup cluster role
-cat <<EOF | kubectl create -f -
-apiVersion: rbac.authorization.k8s.io/v1alpha1
-kind: ClusterRole
-metadata:
-  name: $NAMESPACE-clusterrule
-rules:
-- apiGroups:
-  - extensions
-  resources:
-  - thirdpartyresources
-  verbs:
-  - create
-- apiGroups:
-  - monitoring.kubernetes.io
-  resources:
-  - alertmanagers
-  - prometheuses
-  - servicemonitors
-  verbs:
-  - "*"
-EOF
-
-# bind sa to cluster role
-kubectl create clusterrolebinding $NAMESPACE-clusterrolebinding --clusterrole=$NAMESPACE-clusterrule --serviceaccount=$NAMESPACE:default
+# setup kubectl
+kubectl config set-cluster $SERVICE_ACCOUNT_NAME --embed-certs=true --server=${SERVER_URL} --certificate-authority=./ca.crt
+kubectl config set-credentials $SERVICE_ACCOUNT_NAME --token=$token
+kubectl config set-context $SERVICE_ACCOUNT_NAME --cluster=$SERVICE_ACCOUNT_NAME --user=$SERVICE_ACCOUNT_NAME --namespace=$NAMESPACE
+kubectl config use-context $SERVICE_ACCOUNT_NAME
